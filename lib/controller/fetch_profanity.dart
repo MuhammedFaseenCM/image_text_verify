1f42f1a703877fdfeab2af39b918cf02c962352f
Muhammed Faseen C M <59903311+MuhammedFaseenCM@users.noreply.github.com>
2023-04-28 17:12:23 +0530

    Update fetch_profanity.dart

--- lib/controller/fetch_profanity.dart
+++ lib/controller/fetch_profanity.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'package:image_nudity/controller/text_provider.dart';
 import 'package:image_nudity/model/text_model.dart';
+import 'package:image_nudity/model/api.dart';
 import 'package:provider/provider.dart';
 
 Future<void> fetchProfanity({required text, required context}) async {
@@ -8,7 +9,7 @@ Future<void> fetchProfanity({required text, required context}) async {
   var url = Uri.parse('https://community-purgomalum.p.rapidapi.com/json');
   var headers = {
     'content-type': 'application/octet-stream',
-    'X-RapidAPI-Key': 'b6f5ed5fa0msh7b6952959da336ep173d2ejsne90582df0c9e',
+    'X-RapidAPI-Key': rapidApi,
     'X-RapidAPI-Host': 'community-purgomalum.p.rapidapi.com'
   };
   var params = {'text': '$text'};
